9014039d320e1e907aec703fffda99c138e011b8
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2025-10-23 16:26:16 +0200

    Added

--- rpf/lib/main.dart
+++ rpf/lib/main.dart
@@ -1,122 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const RadioApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RadioApp extends StatelessWidget {
+  const RadioApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Radio Player (Flutter)',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const RadioPlayerScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class RadioPlayerScreen extends StatefulWidget {
+  const RadioPlayerScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<RadioPlayerScreen> createState() => _RadioPlayerScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _RadioPlayerScreenState extends State<RadioPlayerScreen> {
+  late AudioPlayer _player;
+  String? _currentStation;
+  final FirebaseAnalytics _analytics = FirebaseAnalytics.instance;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final List<Map<String, String>> _stations = [
+    {
+      "name": "Sunshine Radio",
+      "url": "https://chmedia.streamabc.net/79-rsunshine-mp3-192-4746851...",
+      "image": "assets/sunshine_logo.png"
+    },
+    {
+      "name": "Radio Argovia",
+      "url": "https://chmedia.streamabc.net/79-argovia-mp3-192-3024993...",
+      "image": "assets/argovia_logo.png"
+    },
+    {
+      "name": "Radio Pilatus",
+      "url": "https://chmedia.streamabc.net/79-pilatus-mp3-192-4664468...",
+      "image": "assets/pilatus_logo.png"
+    },
+    {
+      "name": "SRF 3",
+      "url": "http://livestreaming-node-2.srg-ssr.ch/srgssr/srf3/mp3/128",
+      "image": "assets/srf3_logo.png"
+    },
+    {
+      "name": "SRF 1",
+      "url": "http://livestreaming-node-2.srg-ssr.ch/srgssr/srf1/mp3/128",
+      "image": "assets/srf1_logo.png"
+    },
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _player = AudioPlayer();
+  }
+
+  Future<void> _playStation(String url, String name) async {
+    await _player.setUrl(url);
+    _player.play();
+    setState(() => _currentStation = name);
+
+    // 🔥 Log Firebase Analytics event
+    await _analytics.logEvent(
+      name: "station_played",
+      parameters: {
+        "station_name": name,
+        "timestamp": DateTime.now().toIso8601String(),
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
+  }
+
+  Widget _buildStationRow(Map<String, String> station) {
+    final isCurrent = _currentStation == station["name"];
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(station["image"]!, width: 55, height: 55),
+          const SizedBox(width: 12),
+          SizedBox(
+            width: 150,
+            child: Text(
+              station["name"]!,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 18),
+            ),
+          ),
+          const SizedBox(width: 12),
+          ElevatedButton(
+            onPressed: () => _playStation(station["url"]!, station["name"]!),
+            child: Text(isCurrent ? "Playing…" : "Play"),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: AppBar(title: const Text("Radio Player (Flutter)")),
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: _stations.map(_buildStationRow).toList(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
